d1de7924ec704a2632b80d5d47e32b777c5bc681
Vassilis Asteriou <32526516+VassilisAsteriou@users.noreply.github.com>
2024-10-04 15:55:26 +0300

    Fix GSL test

--- libmp-units/tests/contracts/driver.cpp
+++ libmp-units/tests/contracts/driver.cpp
@@ -1,7 +1,14 @@
-#undef NDEBUG
-#include <cassert>
-
+#include <csignal>
 #include <mp-units/systems/si.h>
+
+
+void signal_handler(int signal)
+{
+  if (signal == SIGABRT)
+  {
+    std::exit(0);
+  }
+}
 
 
 int main ()
@@ -9,7 +16,9 @@ int main ()
   // This test checks that runtime checks are executed.
   // To succeed, this test must exit with SIGABRT
   // ensuring runtime checks are enabled.
-  {
-    MP_UNITS_EXPECTS(false);
-  }
+  auto previous_handler = std::signal(SIGABRT, signal_handler);
+
+  MP_UNITS_EXPECTS(false);
+
+  return 127;
 }
